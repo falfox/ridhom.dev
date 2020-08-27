ada7e1bbbd7d1b8e2fda536924050ec558c470df
Ridho Maulana <edoin21@gmail.com>
2020-08-27 16:07:43 +0700

    Remove whitelistPatterns

--- postcss.config.js
+++ postcss.config.js
@@ -3,7 +3,6 @@ const purgecss = [
   {
     // Specify the paths to all of the template files in your project
     content: ['./pages/**/*.tsx'],
-    whitelistPatterns: [/^/],
     // Include any special characters you're using in this regular expression
     defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
   },
