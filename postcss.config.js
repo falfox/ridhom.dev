6d3f7ad1c2f63e6d5b2c837cd34046b8ad1e8a51
Ridho Maulana <edoin21@gmail.com>
2020-08-27 16:13:52 +0700

    Use built in purgecss from tailwind

--- postcss.config.js
+++ postcss.config.js
@@ -1,17 +1,7 @@
-const purgecss = [
-  '@fullhuman/postcss-purgecss',
-  {
-    // Specify the paths to all of the template files in your project
-    content: ['./pages/**/*.tsx'],
-    // Include any special characters you're using in this regular expression
-    defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-  },
-]
-
 module.exports = {
   plugins: [
     'tailwindcss',
     'autoprefixer',
-    ...(process.env.NODE_ENV === 'production' ? [purgecss, 'cssnano'] : []),
+    ...(process.env.NODE_ENV === 'production' ? ['cssnano'] : []),
   ],
 }
