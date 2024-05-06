91a12c1eab45a2afa4b61c0c65778f7748cd2520
Ridho Maulana <edoin21@gmail.com>
2024-05-06 20:37:16 +0700

    Add redirect for /weep path in next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,16 @@ const nextConfig = {
     eslint: {
         ignoreDuringBuilds: true,
     },
+    async redirects() {
+        return [
+            {
+                source: '/weep',
+                destination: 'https://open.spotify.com/playlist/6ccs74wYPfwnaEtARZ0nKI?si=6fdf12db02ea4a27&pt=435a78bb3644e5c9504b8d2509aed3df',
+                permanent: false,
+            },
+
+        ];
+    },
 };
 
 module.exports = nextConfig;
